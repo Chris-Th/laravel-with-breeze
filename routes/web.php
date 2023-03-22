373f477734fb6090104fc1f52e4c224282355461
Chris-Th <muhmuhmiau@proton.me>
2023-03-22 15:05:18 +0100

    abc

--- routes/web.php
+++ routes/web.php
@@ -28,12 +28,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::post('/create', [MessageController::class, 'create']);
+    // Route::post('/create', [MessageController::class, 'create']);
 });
 
 Route::get('/messages', [MessageController::class, 'showAll']);
 
 Route::post('/create', [MessageController::class, 'create']);
+
+Route::get('/message/{id}', [MessageController::class, 'details']);
+
+Route::delete('/message/{id}', [MessageController::class, 'delete']);
+
+
 
 
  
